c843cfcadd1d80a0ad3b5ece87d1326f435e0b45
Aditya_Sharma-ITT <aditya.s@intimetec.com>
2024-04-26 10:44:34 +0530

    fix: made some changes based on feedback

---
+++ Week-4/Daily-Assignments/mongoSchemaAndConnection/index.ts
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const isLocalDatabase = false;
+
+const useMongoDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(isLocalDatabase ? process.env.LOCAL_DB : process.env.CLOUD_DB);
+    const playerSchema = new mongoose.Schema(
+      {
+        name: String,
+        sport: String,
+      },
+      { collection: "players" }
+    );
+    const PlayerModel = new mongoose.model("PlayerModel", playerSchema);
+    const players = await PlayerModel.find();
+    console.log(players);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+useMongoDB();
